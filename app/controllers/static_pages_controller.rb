7cf373fe5fca2d1d274c022a6dcddebe7780dc94
Nurzhas <nurzha55s@gmail.com>
2014-08-19 15:07:57 +0000

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  def home
+    <h1>StaticPages#home</h1>
+<p>Find me in app/views/static_pages/home.html.erb</p>
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
