c22f81b539aa4d64db9890a37ce2fc352ae3b590
Nurzhas <nurzha55s@gmail.com>
2014-09-02 14:38:51 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,6 +11,10 @@ class User < ActiveRecord::Base
 
     def User.new_remember_token
     SecureRandom.urlsafe_base64
+  end
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
   end
 
   def User.encrypt(token)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp::Application.routes.draw do
-  
+  resources :users
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
     root  'static_pages#home'
   
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
 
   
   match '/signin',  to: 'sessions#new', via: 'get'
